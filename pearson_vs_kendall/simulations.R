6727471e10d129582e0227cc473b8bedd41c620b
fdoel <595906fd@student.eur.nl>
2023-08-14 11:10:06 +0200

    multiple dimension loop added

--- pearson_vs_kendall/simulations.R
+++ pearson_vs_kendall/simulations.R
@@ -1,6 +1,8 @@
 # ------------------------------------
 # Author: Andreas Alfons
 #         Erasmus University Rotterdam
+#         Floris van den Doel
+#         Erasmus University Rotterdam
 # ------------------------------------
 
 
@@ -12,19 +14,13 @@ library("ltm")
 
 # control parameters for data generation
 n <- 1000                               # number of observations
-p <- 2                                  # number of items
+p_low <- 2                              # minimal number of items
+p_high <- 2                             # maximal number of items
 prob <- c(0.05, 0.25, 0.4, 0.25, 0.05)  # probabilities of response categories
 L <- length(prob)                       # number of response categories
 rho <- 0.7                              # target correlation between items
 R <- 10                                 # number of simulation runs
 seed <- 20230111                        # seed of the random number generator
-
-# define matrix with probabilities of response categories per item
-prob_mat <- matrix(prob, nrow = p, ncol = L, byrow = TRUE)
-
-# define correlation matrix
-Rho <- matrix(rho, nrow = p, ncol = p)
-diag(Rho) <- 1
 
 # it is very easy to use parallel computing on Unix systems, but not on Windows
 if (.Platform$OS.type == "windows") {
@@ -34,73 +30,107 @@ if (.Platform$OS.type == "windows") {
   RNGkind("L'Ecuyer-CMRG")  # use parallel random number streams
 }
 
-
 # run simulation
 cat(paste(Sys.time(), ": starting ...\n"))
 set.seed(seed)
-results_list <- parallel::mclapply(seq_len(R), function(r) {
 
-  # print simulation run
-  cat(paste(Sys.time(), sprintf(":   run = %d\n", r)))
+p_results_list <- parallel::mclapply(seq(p_low, p_high), function(p) {
+  # define matrix with probabilities of response categories per item
+  prob_mat <- matrix(prob, nrow = p, ncol = L, byrow = TRUE)
 
-  # initialize data table
-  initial <- simstudy::genData(n)
-  # generate correlated rating-scale items
-  data <- simstudy::genOrdCat(initial, baseprobs = prob_mat, prefix = "item",
+  # define correlation matrix
+  Rho <- matrix(rho, nrow = p, ncol = p)
+  diag(Rho) <- 1
+  
+  results_list <- parallel::mclapply(seq_len(R), function(r) {
+    # print simulation run
+    cat(paste(Sys.time(), sprintf(":   run = %d\n", r)))
+
+    # initialize data table
+    initial <- simstudy::genData(n)
+    # generate correlated rating-scale items
+    data <- simstudy::genOrdCat(initial, baseprobs = prob_mat, prefix = "item",
                               corMatrix = Rho)
-  # drop ID and convert to integer matrix
-  data <- as.matrix(data[, -1])
-  storage.mode(data) <- "integer"
+    # drop ID and convert to integer matrix
+    data <- as.matrix(data[, -1])
+    storage.mode(data) <- "integer"
 
 
-  # compute Pearson correlation
-  df_pearson <- tryCatch({
-    pearson <- ccaPP::corPearson(data[, 1], data[, 2])
-    data.frame(Run = r, Method = "Pearson",
-                 Correlation = pearson)
-  }, error = function(e) NULL, warning = function(w) NULL)
+    # compute Pearson correlation
+    df_pearson <- tryCatch({
+      pearson_matrix = cor(data, method = c("pearson"))
+      data.frame(Items = p, Run = r, Method = "Pearson",
+              Correlation = pearson_matrix)
+    }, error = function(e) NULL, warning = function(w) NULL)
+    # compute Kendall correlation
+    df_kendall <- tryCatch({
+      kendall_matrix <- cor(data, method = c("kendall"))
+      data.frame(Items = p, Run = r, Method = "Kendall",
+              Correlation = kendall_matrix)
+    }, error = function(e) NULL, warning = function(w) NULL)
 
-  # compute Kendall correlation
-  df_kendall <- tryCatch({
-    kendall <- ccaPP::corKendall(data[, 1], data[, 2])
-    data.frame(Run = r, Method = "Kendall",
-            Correlation = kendall)
-  }, error = function(e) NULL, warning = function(w) NULL)
+    # combine results
+    rbind(df_pearson, df_kendall) 
+    
 
-  # combine results
-  rbind(df_pearson, df_kendall)
+}, mc.cores = n_cores)
+# combine results into data frame
+results <- do.call(rbind, results_list)
 
-  # combine results from current simulation run into data frame
-  do.call(rbind)
+# aggregate results over the simulation runs
+
+library("dplyr")
+
+#First calculate the average correlation matrix under Pearson
+pearson = data.matrix(filter(results, Method == "Pearson")[-1:-3])
+print(pearson)
+X <- matrix(0, ncol = p, nrow = p)
+for(i in seq(1, (R*p), p)) {
+  X <- X + pearson[i:(i+p-1), 1:p]
+}
+
+aggregated_pearson = data.frame(Items = p, Method = "Pearson", Covariance = X/R)
+
+# Do the same for Kendall correlation
+
+kendall = data.matrix(filter(results, Method == "Kendall")[-1:-3])
+Y <<- matrix(0, ncol = p, nrow = p)
+
+# p*p matrices therefore
+
+
+for(j in seq(1, R*p, p)) {
+  Y <- Y + kendall[j:(j+p-1), 1:p]
+}
+
+aggregated_kendall = data.frame(Items = p, Method = "Kendall", Covariance = Y/R)
+
+#Combine the results into a data frame
+rbind(aggregated_pearson, aggregated_kendall)
+
 
 }, mc.cores = n_cores)
 
-# combine results into data frame
-results <- do.call(rbind)
-
-# save results to file
-file_results <- "simulations_single_script/results/results_n=%d.RData"
-save(results, n, p, prob, rho, seed, file = sprintf(file_results, n))
 
 # print message that simulation is done
-cat(paste(Sys.time(), ": finished.\n"))
+#cat(paste(Sys.time(), ": finished.\n"))
 
-
-# aggregate results over the simulation runs
-library("dplyr")
-aggregated <- results %>%
-  group_by(epsilon, Method) %>%
-  summarize(Correlation = mean(Correlation),
-            .groups = "drop")
 
 # plot average results over the simulation runs
-library("ggplot2")
-p <- ggplot() +
-  geom_line(aes(x = epsilon, y = Correlation, color = Method),
-            data = aggregated)
+#library("ggplot2")
+#p <- ggplot() +
+#  geom_line(aes(x = p, y = Correlation, color = Method),
+#            data = aggregated)
 
 # save plot to file
-file_plot <- "simulations_single_script/figures/results_n=%d.pdf"
-pdf(file = sprintf(file_plot, n), width = 5, height = 3.5)
-print(p)
+#file_plot <- "C:/Users/flori/simulations-practice/pearson_vs_kendall/results/results_n=%d.pdf"
+#pdf(file = sprintf(file_plot, n), width = 5, height = 3.5)
+#print(p)
+
+
+
+# save results to file
+file_results <- "C:/Users/flori/simulations-practice/pearson_vs_kendall/results/results_n=%d.RData"
+save(results, n, p, prob, rho, seed, file = sprintf(file_results, n))
+
 dev.off()
