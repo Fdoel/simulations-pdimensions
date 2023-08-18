541079f79d028cd78be382845c63a42e8fa7d298
Floris <floris@floriss-air.kpn>
2023-08-18 11:55:09 +0200

    Added a script for variable amount of categories

---
+++ pearson_vs_kendall/varied_L.R
@@ -0,0 +1,135 @@
+# ------------------------------------
+# Author: Andreas Alfons
+#         Erasmus University Rotterdam
+#         Floris van den Doel
+#         Erasmus University Rotterdam
+# ------------------------------------
+
+
+# load packages, but functions should still be called with package::function()
+library("parallel")
+library("simstudy")
+library("ccaPP")
+
+# Function to generate base proabilities with multiple categories
+probabilty_likert <- function(bins, likert_mean = (1 + bins)/2, likert_sd = bins/4) {
+  intervals = c(seq(1.5, bins - 0.5), Inf)
+  x <- pnorm(intervals, mean = likert_mean, sd = likert_sd)
+  y <- dplyr::lag(x, default = 0)
+  x - y
+}
+
+# control parameters for data generation
+n <- 1000                               # number of observations
+p <- 2                                  # number of items
+rho <- 0.8                              # target correlation between items
+R <- 100                                 # number of simulation runs
+seed <- 20230111                        # seed of the random number generator
+l_increment= 5                          # increment for number of categories
+l_max = 1000                             # max amount of categories
+L <- seq(5, l_max, l_increment)
+
+# define correlation matrix
+Rho <- matrix(rho, nrow = p, ncol = p)
+diag(Rho) <- 1
+
+# it is very easy to use parallel computing on Unix systems, but not on Windows
+if (.Platform$OS.type == "windows") {
+  n_cores <- 1              # use only one CPU core
+} else {
+  n_cores <- 2              # number of CPU cores to be used
+  RNGkind("L'Ecuyer-CMRG")  # use parallel random number streams
+}
+
+# run simulation
+cat(paste(Sys.time(), ": starting ...\n"))
+set.seed(seed)
+
+results_list_L <- parallel::mclapply(L, function(l) {
+  
+  # define matrix with probabilities of response categories per item
+  prob <- probabilty_likert(l)
+  prob_mat <- matrix(prob, nrow = p, ncol = l, byrow = TRUE)
+
+  results_list <- parallel::mclapply(seq_len(R), function(r) {
+    
+    # print simulation run
+    cat(paste(Sys.time(), sprintf(": L=%d\n", l), sprintf(":   run = %d\n", r)))
+    
+    # initialize data table
+    initial <- simstudy::genData(n)
+    # generate correlated rating-scale items
+    data <- simstudy::genOrdCat(initial, baseprobs = prob_mat, prefix = "item",
+                                corMatrix = Rho)
+    # drop ID and convert to integer matrix
+    data <- as.matrix(data[, -1])
+    storage.mode(data) <- "integer"
+    
+    # compute Pearson correlation
+    df_pearson <- tryCatch({
+      pearson <- ccaPP::corPearson(data[, 1], data[, 2])
+      data.frame(Run = r, Categories = l, Method = "Pearson",
+                 Correlation = pearson)
+    }, error = function(e) NULL, warning = function(w) NULL)
+    
+    # compute Kendall correlation
+    df_kendall <- tryCatch({
+      kendall <- ccaPP::corKendall(data[, 1], data[, 2])
+      data.frame(Run = r, Categories = l, Method = "Kendall",
+                 Correlation = kendall)
+    }, error = function(e) NULL, warning = function(w) NULL)
+    
+    df_transformed_kendall <- tryCatch({
+      kendall <- ccaPP::corKendall(data[, 1], data[, 2])
+      transformed_kendall <- sin(kendall*pi/2)
+      data.frame(Run = r, Categories = l, Method = "Transformed Kendall",
+                 Correlation = transformed_kendall)
+      
+    }, error = function(e) NULL, warning = function(w) NULL)
+    
+    # combine results
+    rbind(df_pearson, df_kendall, df_transformed_kendall)
+    
+  }, mc.cores = n_cores)
+  
+  # combine results into data frame
+  results <- do.call(rbind, results_list)
+  
+}, mc.cores = n_cores)
+
+# combine results into data frame
+results_L <- do.call(rbind, results_list_L)
+
+# aggregate results over the simulation runs
+library("dplyr")
+aggregated <- results_L %>%
+  group_by(Categories, Method) %>%
+  summarize(Correlation = mean(Correlation),
+            .groups = "drop")
+
+
+
+# save results to file
+file_path_results = "Pearson_vs_kendall/results/varied_L/results"
+file_info = paste(
+  sprintf("n=%d", n),
+  sprintf("max=%d", l_max),
+  sprintf("increment=%d", l_increment),
+  sep = "_")
+
+save(results_L, n, p, rho, l_max, l_increment, seed, file = paste(file_path_results, file_info, ".RData", sep = ""))
+
+# print message that simulation is done
+cat(paste(Sys.time(), ": finished.\n"))
+
+# plot average results over the simulation runs
+library("ggplot2")
+p <- ggplot() +
+  geom_line(aes(x = Categories, y = Correlation, color = Method),
+            data = aggregated)
+
+# save plot to file
+file_path_plot = "pearson_vs_kendall/figures/varied_L/results"
+pdf(file = paste(file_path_plot, file_info, ".pdf", sep = ""), width = 5, height = 3.5)
+print(p)
+dev.off()
--- pearson_vs_kendall/varied_rho.R
+++ pearson_vs_kendall/varied_rho.R
@@ -1,6 +1,8 @@
 # ------------------------------------
 # Author: Andreas Alfons
 #         Erasmus University Rotterdam
+#         Floris van den Doel
+#         Erasmus University Rotterdam
 # ------------------------------------
 
 
@@ -13,7 +15,7 @@ library("ccaPP")
 # control parameters for data generation
 n <- 10000                                # number of observations
 p <- 2                                  # number of items
-prob <- c(0.05, 0.075, 0.1, 0.125, 0.3, 0.125, 0.1, 0.075, 0.05)  # probabilities of response categories
+prob <- c(0.05, 0.15, 0.4, 0.15, 0.05)  # probabilities of response categories
 L <- length(prob)                       # number of response categories
 rho_seq <- seq(0.2, 0.8, 0.1)           # target correlation between items
 R <- 100                                 # number of simulation runs
@@ -36,7 +38,7 @@ set.seed(seed)
 
 #Loop over different target correlations
 
-rho_results_list <- parallel::mclapply(rho_seq, function(rho) {
+results_list_rho <- parallel::mclapply(rho_seq, function(rho) {
 
   # define correlation matrix
   Rho <- matrix(rho, nrow = p, ncol = p)
@@ -79,17 +81,18 @@ rho_results_list <- parallel::mclapply(rho_seq, function(rho) {
 }, mc.cores = n_cores)
   
 # combine results into data frame
-rho_results <- do.call(rbind, rho_results_list)
+results_rho <- do.call(rbind, results_list_rho)
 
 # aggregate results over the simulation runs
 library("dplyr")
-aggregated <- rho_results %>%
-  group_by(Target, Method) %>%
+aggregated <- results_rho %>%
+  group_by(Category, Method) %>%
   summarize(Correlation = mean(Correlation),
             .groups = "drop")
+
 # save results to file
-file_results <- "C:/Users/flori/simulations-practice/pearson_vs_kendall/results/9cat/results_n=%d.RData"
-save(rho_results, n, p, prob,rho_seq ,seed, file = sprintf(file_results, n))
+file_results <- "Pearson_vs_kendall/results/varied_rho/results_n=%d.RData"
+save(results_rho, n, p ,prob, rho_seq, seed, file = sprintf(file_results, n))
 
 # print message that simulation is done
 cat(paste(Sys.time(), ": finished.\n"))
@@ -102,7 +105,7 @@ p <- ggplot() +
             data = aggregated)
 
 # save plot to file
-file_plot <- "C:/Users/flori/simulations-practice/pearson_vs_kendall/figures/9cat/results_n=%d.pdf"
+file_plot <- "C:/Users/flori/simulations-practice/pearson_vs_kendall/figures/varied_rho/results_n=%d.pdf"
 pdf(file = sprintf(file_plot, n), width = 5, height = 3.5)
 print(p)
 dev.off()
