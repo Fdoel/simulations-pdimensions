85c94ea6962f767529386d17b1488736c6ea6458
Floris <floris@floriss-air.kpn>
2023-08-19 09:15:23 +0200

    Added careless respondents back in plus clusters

--- pearson_vs_kendall/varied_L.R
+++ pearson_vs_kendall/varied_L.R
@@ -11,7 +11,7 @@ library("parallel")
 library("simstudy")
 library("ccaPP")
 
-# Function to generate base proabilities with multiple categories
+# Function to generate base probabilities with multiple categories
 probabilty_likert <- function(bins, likert_mean = (1 + bins)/2, likert_sd = bins/4) {
   intervals = c(seq(1.5, bins - 0.5), Inf)
   x <- pnorm(intervals, mean = likert_mean, sd = likert_sd)
@@ -28,6 +28,10 @@ seed <- 20230111                        # seed of the random number generator
 l_increment= 2                          # increment for number of categories
 l_max = 30                              # max amount of categories
 L <- seq(3, l_max, l_increment)
+
+# control parameters for random respondents
+epsilons <- seq(0, 0.3, by = 0.3)
+epsilon_max <- max(epsilons)
 
 # define correlation matrix
 Rho <- matrix(rho, nrow = p, ncol = p)
@@ -53,7 +57,7 @@ results_list_L <- parallel::mclapply(L, function(l) {
   results_list <- parallel::mclapply(seq_len(R), function(r) {
     # print simulation run
     cat(paste(Sys.time(), sprintf(": L=%d\n", l), sprintf(":   run = %d\n", r)))
-    
+
     # initialize data table
     initial <- simstudy::genData(n)
     # generate correlated rating-scale items
@@ -63,30 +67,70 @@ results_list_L <- parallel::mclapply(L, function(l) {
     data <- as.matrix(data[, -1])
     storage.mode(data) <- "integer"
     
-    # compute Pearson correlation
-    df_pearson <- tryCatch({
-      pearson <- ccaPP::corPearson(data[, 1], data[, 2])
-      data.frame(Run = r, Categories = l, Method = "Pearson",
-                 Correlation = pearson)
-    }, error = function(e) NULL, warning = function(w) NULL)
+    # generate probabilities of being a random respondents
+    careless_probabilities <- runif(n)
     
-    # compute Kendall correlation
-    df_kendall <- tryCatch({
-      kendall <- ccaPP::corKendall(data[, 1], data[, 2])
-      data.frame(Run = r, Categories = l, Method = "Kendall",
-                 Correlation = kendall)
-    }, error = function(e) NULL, warning = function(w) NULL)
+    # order observations according to probabilities of being random respondents,
+    # which makes it easier to keep previous careless respondents the same as the
+    # contamination level increases (for maximum comparability)
+    order <- order(careless_probabilities)
+    data <- data[order, ]
+    careless_probabilities <- careless_probabilities[order]
     
-    df_transformed_kendall <- tryCatch({
-      kendall <- ccaPP::corKendall(data[, 1], data[, 2])
-      transformed_kendall <- sin(kendall*pi/2)
-      data.frame(Run = r, Categories = l, Method = "Transformed Kendall",
-                 Correlation = transformed_kendall)
+    # generate clustered responses to be used for careless respondents
+    n_careless_max <- sum(careless_probabilities < epsilon_max)
+    careless_prob_matrix <- probabilty_likert(l, l/1.1) %>%
+      matrix(nrow = p, ncol = l, byrow = TRUE)
+    data_careless <- simstudy::genData(n_careless_max) %>%
+      simstudy::genOrdCat(baseprobs = careless_prob_matrix, prefix = "item", corMatrix = Rho)
+    data_careless <- as.matrix(data_careless[, -1])
+    storage.mode(data_careless) <- "integer"
+    
+    # In case you want to go for careless opposed to clustered data
+    # data_careless <- replicate(p, sample.int(l, n_careless_max, replace = TRUE))
+    
+    
+    # loop over contamination levels
+    results_r <- lapply(epsilons, function(epsilon) {
       
-    }, error = function(e) NULL, warning = function(w) NULL)
+      # turn selected observations into careless respondents: since the
+      # observations are sorted according to the probability of being careless,
+      # this keeps previous careless respondents the same as the contamination
+      # level increases
+      if (epsilon > 0) {
+        careless <- which(careless_probabilities < epsilon)
+        data[careless, ] <- data_careless[careless, ]
+      }
+      
+      # compute Pearson correlation
+      df_pearson <- tryCatch({
+        pearson <- ccaPP::corPearson(data[, 1], data[, 2])
+        data.frame(Run = r, Epsilon = epsilon, Categories = l, Method = "Pearson",
+                  Correlation = pearson)
+      }, error = function(e) NULL, warning = function(w) NULL)
     
-    # combine results
-    rbind(df_pearson, df_kendall, df_transformed_kendall)
+      # compute Kendall correlation
+      df_kendall <- tryCatch({
+        kendall <- ccaPP::corKendall(data[, 1], data[, 2])
+        data.frame(Run = r, Epsilon = epsilon, Categories = l, Method = "Kendall",
+                   Correlation = kendall)
+      }, error = function(e) NULL, warning = function(w) NULL)
+      
+      df_transformed_kendall <- tryCatch({
+        kendall <- ccaPP::corKendall(data[, 1], data[, 2])
+        transformed_kendall <- sin(kendall*pi/2)
+        data.frame(Run = r, Epsilon = epsilon, Categories = l, Method = "Transformed Kendall",
+                   Correlation = transformed_kendall)
+        
+      }, error = function(e) NULL, warning = function(w) NULL)
+      
+      # combine results
+      rbind(df_pearson, df_kendall, df_transformed_kendall)
+    
+    })
+    
+    # combine results from current simulation run into data frame
+    do.call(rbind, results_r)
     
   }, mc.cores = n_cores)
   
@@ -101,11 +145,9 @@ results_L <- do.call(rbind, results_list_L)
 # aggregate results over the simulation runs
 library("dplyr")
 aggregated <- results_L %>%
-  group_by(Categories, Method) %>%
+  group_by(Categories, Epsilon, Method) %>%
   summarize(Correlation = mean(Correlation),
             .groups = "drop")
-
-
 
 # save results to file
 file_path_results = "Pearson_vs_kendall/results/varied_L/results"
@@ -121,13 +163,25 @@ save(results_L, n, p, rho, l_max, l_increment, seed, file = paste(file_path_resu
 cat(paste(Sys.time(), ": finished.\n"))
 
 # plot average results over the simulation runs
-library("ggplot2")
-p <- ggplot() +
-  geom_line(aes(x = Categories, y = Correlation, color = Method),
-            data = aggregated)
 
-# save plot to file
+library("ggplot2")
+plot_box <-  dplyr::filter(results_L, near(Epsilon, 0.0)) %>%
+  ggplot(aes(x = factor(Categories), y = Correlation, color = Method)) +
+    geom_boxplot()
+
+
+plot_line <- ggplot(results_L, aes(x = Categories, y = Correlation, color = Method)) +
+  geom_smooth(aes(linetype = factor(Epsilon)))
+
+# save box plot to file
 file_path_plot = "pearson_vs_kendall/figures/varied_L/results"
-pdf(file = paste(file_path_plot, file_info, ".pdf", sep = ""), width = 5, height = 3.5)
-print(p)
+pdf(file = paste(file_path_plot, file_info, "box", ".pdf", sep = ""), width = 5, height = 3.5)
+print(plot_box)
 dev.off()
+
+# save line plot to file
+file_path_plot = "pearson_vs_kendall/figures/varied_L/results"
+pdf(file = paste(file_path_plot, file_info, "line", ".pdf", sep = ""), width = 5, height = 3.5)
+print(plot_line)
+dev.off()
+print(plot_line)
