3d1c0a4383900fe822225be5814068bfe72e192b
Floris <floris@Floriss-MacBook-Air.local>
2023-09-19 10:32:34 +0200

    Added option for different probability distributions

--- functions/probabilities.R
+++ functions/probabilities.R
@@ -11,12 +11,21 @@ likert_sd <- function(x) {
 }
 
 # Function to get likert base probabilities
-likert_prob <- function(bins, likert_mean = (1 + bins)/2, likert_sd = bins/4) {
-  intervals = c(seq(1.5, bins - 0.5), Inf)
-  x <- pnorm(intervals, mean = likert_mean, sd = likert_sd)
-  y <- dplyr::lag(x, default = 0)
-  x - y
-}
+likert_prob <-
+  function(bins,
+           likert_mean = (1 + bins) / 2,
+           likert_sd = bins / 4,
+           method = "norm") {
+    intervals = c(seq(1.5, bins - 0.5), Inf)
+    x <- switch (
+      method,
+      "norm" =  pnorm(intervals, mean = likert_mean, sd = likert_sd),
+      "unif" = punif(seq(1, bins), 0, bins),
+      "cauchy" = pcauchy(intervals, likert_mean, likert_sd)
+    )
+    y <- dplyr::lag(x, default = 0)
+    x - y
+  }
 
 mamd <- function(x) {
   mean_x <- mean(x)
@@ -25,3 +34,6 @@ mamd <- function(x) {
   }
   median(x)/0.675
 }
+
+test1 <- likert_prob(5, method = "cauchy")
+
